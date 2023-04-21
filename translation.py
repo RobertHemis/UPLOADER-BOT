477760bec69a04354e0f285e4fd3d4636cc64126
Robert Hemis <130218888+RobertHemis@users.noreply.github.com>
2023-04-21 17:31:48 +0600

    Update translation.py

--- translation.py
+++ translation.py
@@ -3,18 +3,18 @@ class Translation(object):
     ERROR = "<b>ERROR :</b> {}"
 
     START_TEXT = """Hi {},
-I'm URL X Uploader!
+I'm Alifa!
 You can upload HTTP/HTTPS direct link, Using this bot!
 
 /help for more details!"""
     FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> \nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons.\nYou can use /deletethumbnail to delete the auto-generated thumbnail."
     SET_CUSTOM_USERNAME_PASSWORD = """If you want to download premium videos, provide in the following format:
 URL | filename | username | password"""
-    DOWNLOAD_START = "📥Downloading..."
-    UPLOAD_START = "📤Uploading..."
+    DOWNLOAD_START = "Downloading..."
+    UPLOAD_START = "Uploading..."
     RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 2GB due to Telegram API limitations."
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "Thanks for using @Uploader_X_bot\n\n<b>Join : @Space_X_Bots</b>"
-    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds.\n\n@Uploader_X_Bot"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "Thanks for using @alifano02bot"
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds."
     SAVED_CUSTOM_THUMB_NAIL = "Custom video / file thumbnail saved. This image will be used in the video / file."
     DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Custom thumbnail cleared succesfully."
     CUSTOM_CAPTION_UL_FILE = "{}"
@@ -24,12 +24,12 @@ URL | filename | username | password"""
 1. Send url (example.domain/File.mp4 | New Filename.mp4).
 2. Send Image As Custom Thumbnail (Optional).
 3. Select the button.
-   SVideo - Give File as video with Screenshots
-   DFile  - Give File (video) as file with Screenshots
-   Video  - Give File as video without Screenshots
-   File   - Give File without Screenshots
+   SVideo - Give File as video with screenshots
+   DFile  - Give File (video) as file with screenshots
+   Video  - Give File as video without screenshots
+   File   - Give File without screenshots
 
-If bot didn't respond, contact @Sources_Codes"""
+If bot didn't respond, contact @abeseil"""
     REPLY_TO_MEDIA_ALBUM_TO_GEN_THUMB = "Reply /generatecustomthumbnail to a media album, to generate custom thumbail"
     ERR_ONLY_TWO_MEDIA_IN_ALBUM = """Media Album should contain only two photos. Please re-send the media album, and then try again, or send only two photos in an album."
 You can use /rename command after receiving file to rename it with custom thumbnail support.
